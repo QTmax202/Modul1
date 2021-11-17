c2be5e11de7224cf1d75107681aa65fb1d785403
QTmax202 <89342545+QTmax202@users.noreply.github.com>
2021-11-17 14:25:13 +0700

    MODUL1

---
+++ Variables ,Data Types and Operators/Practice/using-the-operators-js.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("the width");
+inputHeight = prompt("the Height");
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let area = Width * Height ;
+document.write("the area"+area);
